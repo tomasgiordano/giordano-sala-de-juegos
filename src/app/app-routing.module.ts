6094f0e0977e401996b1f1395c35353cbf698aa0
TomasGiordano <tomas.giordano@boreal-it.com>
2021-03-22 10:38:50 -0300

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full"
+  },
+  {
+    component: LoginComponent,
+    path:"login"
+  },
+  {
+    component: HomeComponent,
+    path: "home"
+  },
+  {
+    component: QuienSoyComponent,
+    path: "quien-soy"
+  },
+  {
+    component: ErrorComponent,
+    path:"**"
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    QuienSoyComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
